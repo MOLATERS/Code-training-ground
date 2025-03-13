2ad34bef46ef4496aac7e9bbeb715853d73a166b
MOLATERS <1643381107@qq.com>
2025-03-13 10:20:19 +0800

    判断二叉树是否相等

---
+++ leetcode/100.相同的树.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=cpp
+ *
+ * [100] 相同的树
+ */
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) {
+            return true;
+        } else if (p == nullptr || q == nullptr) {
+            return false;
+        } else if (p->val != q->val) {
+            return false;
+        } else {
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        }
+    }
+};
