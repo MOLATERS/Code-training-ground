93946971fe9850630fed55334036d19fc27d3d40
MOLATERS <1643381107@qq.com>
2025-03-15 09:46:42 +0800

    最小高度

---
+++ leetcode/111.二叉树的最小深度.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=111 lang=cpp
+ *
+ * [111] 二叉树的最小深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == nullptr) return 0; // 表示空结点
+        if(root->right == nullptr && root->left == nullptr) return 1; // 表示叶节点
+        int min_depth = INT_MAX;
+        if(root->left != nullptr) min_depth = min(min_depth, minDepth(root->left));
+        if(root->right != nullptr) min_depth = min(min_depth, minDepth(root->right));
+        return min_depth+1;
+    }
+};
+// @lc code=end
+
